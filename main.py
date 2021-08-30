970c92e805f0cc9adba911d11a167afe0464b922
Dzen819 <43151543+Dzen819@users.noreply.github.com>
2021-08-30 18:58:33 +0300

    final

--- main.py
+++ main.py
@@ -4,7 +4,38 @@ import playground
 window = Tk()
 
 window.title("Converter")
-window.minsize(height=300, width=500)
+window.minsize(height=100, width=230)
+window.config(padx=10, pady=10)
+
+
+def calculate():
+    try:
+        inp = int(input.get())
+        mls = round(inp/1.6, 2)
+        calc_text.config(text=mls)
+    except:
+        calc_text.config(text="Это не число")
+
+
+text1 = Label(text="равно")
+text1.grid(column=0, row=1)
+
+text2 = Label(text="Миль")
+text2.grid(column=2, row=1)
+
+text2 = Label(text="Км")
+text2.grid(column=2, row=0)
+
+calc_text = Label(text="0")
+calc_text.grid(column=1, row=1)
+
+button = Button(text="Calculate", command=calculate)
+button.grid(column=1, row=2)
+
+input = Entry()
+input.insert(0, "0")
+input.grid(column=1, row=0)
+
 
 
 
